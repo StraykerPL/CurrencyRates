81afeaf64b4c855912a5c9bca58f6484f3310a3b
Strayker <45916090+StraykerPL@users.noreply.github.com>
2025-06-04 23:16:02 +0200

    Add base version of backend's endpoints

--- currency-rates-api/main.py
+++ currency-rates-api/main.py
@@ -1,12 +1,39 @@
 from typing import Union
 from fastapi import FastAPI
+import datetime
+import requests
+import json
+import mysql.connector
 
 app = FastAPI()
+dbContext = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="currency_rates"
+)
+cursor = dbContext.cursor()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/currencies")
+def read_currencies():
+    cursor.execute("SELECT code FROM rates")
+    finalResult = ""
+    for row in cursor.fetchall():
+        finalResult += json.loads(row)
+    return finalResult
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/currencies/{dateStart}/{dateEnd}")
+def read_currencies_date(dateStart: str, dateEnd: str):
+    res = requests.get(f'https://api.nbp.pl/api/exchangerates/tables/A/{dateStart}/{dateEnd}?format=json')
+    return json.loads(res.text)
+
+@app.post("/currencies/fetch")
+def write_save_to_db():
+    response = requests.get('https://api.nbp.pl/api/exchangerates/tables/A?format=json')
+    result = json.loads(response.text)
+    query = "INSERT INTO rates (name, code, rate) VALUES (%s, %s, %s)"
+    cursor.execute("DELETE * FROM rates")
+    for currency in result.rates:
+        values = (currency.currency, currency.code, currency.mid)
+        cursor.execute(query, values)
+    dbContext.commit()
